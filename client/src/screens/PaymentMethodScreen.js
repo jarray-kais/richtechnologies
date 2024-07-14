d426d7e31710a1267011b25056de380f55f7ee62
jarray-kais <jarraykais1@gmail.com>
2024-07-14 10:58:01 +0100

    Implement PaymentMethodScreen for selecting payment method - Create PaymentMethodScreen component to allow users to choose their preferred payment method - Utilize React hooks useState and useContext for state management and interaction with the application context - Integrate CheckoutSteps component to display current step in the checkout process - Dispatch action to save selected payment method to application state upon submission - Navigate to the place order screen upon successful selection
    Signed-off-by: jarray-kais <jarraykais1@gmail.com>

---
+++ client/src/screens/PaymentMethodScreen.js
@@ -0,0 +1,77 @@
+
+import { useState, useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { Store } from '../Context/CartContext';
+import CheckoutSteps from '../components/Chekout/CheckoutSteps';
+import BackButton from '../components/BackButton/BackButton';
+
+
+const PaymentMethodScreen = () => {
+  const navigate = useNavigate();
+  const { state, dispatch: ctxDispatch } = useContext(Store);
+  const { paymentMethod } = state;
+  const [selectedPaymentMethod, setSelectedPaymentMethod] = useState(paymentMethod || 'Cash on Delivery');
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    ctxDispatch({ type: 'SAVE_PAYMENT_METHOD', payload: selectedPaymentMethod });
+    navigate('/placeorder');
+  };
+
+  return (
+    <div className="shipping-container ">
+    <CheckoutSteps step1 step2 step3 />
+    <div className="shipping-body ">
+    <BackButton />
+      <form className="pay-form" onSubmit={submitHandler}>
+        <h1 className="pay-form-title">Méthode de paiement</h1>
+        <div className="pay-form-group">
+        
+            <input
+              type="radio"
+              id="cashOnDelivery"
+              value="Cash on Delivery"
+              checked={selectedPaymentMethod === 'Cash on Delivery'}
+              onChange={(e) => setSelectedPaymentMethod(e.target.value)}
+            />
+            <label htmlFor="cashOnDelivery">  Cash on Delivery
+          </label>
+        </div>
+        <div className="pay-form-group">
+          
+            <input
+              type="radio"
+              id="stripe"
+              value="Stripe"
+              checked={selectedPaymentMethod === 'Stripe'}
+              onChange={(e) => setSelectedPaymentMethod(e.target.value)}
+            />
+            <label htmlFor="stripe">
+            Stripe
+          </label>
+        </div>
+        <div className="pay-form-group">
+          
+            <input
+              type="radio"
+              id="flouci"
+              value="Flouci"
+              checked={selectedPaymentMethod === 'Flouci'}
+              onChange={(e) => setSelectedPaymentMethod(e.target.value)}
+            />
+            <label htmlFor="flouci">
+            Flouci
+          </label>
+        </div>
+        <div className="pay-action-buttons">
+          <button type="submit" className="primary">
+            Continuer
+          </button>
+        </div>
+      </form>
+    </div>
+    </div>
+  );
+};
+
+export default PaymentMethodScreen;
